85112dae3d808b76819b2fe0fc0ba363286c5fe7
Jean-Dv <mrjunior127@gmail.com>
2023-12-02 17:43:00 -0500

    feat(infrastructure): update repository for matching with criteria in seasons

--- src/Contexts/Multimedia/Season/domain/SeasonRepository.ts
+++ src/Contexts/Multimedia/Season/domain/SeasonRepository.ts
@@ -1,3 +1,4 @@
+import { type Criteria } from '@Shared/domain/criteria/Criteria'
 import { type Season } from './Season'
 
 export interface SeasonRepository {
@@ -7,4 +8,12 @@ export interface SeasonRepository {
    * @param season The season entity to be saved.
    */
   save: (season: Season) => Promise<void>
+
+  /**
+   * Retrieves seasons that match the specified criteria.
+   *
+   * @param criteria - The criteria used for matching seasons.
+   * @returns A promise that resolves to an array of matched seasons.
+   */
+  matching: (criteria: Criteria) => Promise<Season[]>
 }
--- src/Contexts/Multimedia/Season/infrastructure/persistence/MongoSeasonRepository.ts
+++ src/Contexts/Multimedia/Season/infrastructure/persistence/MongoSeasonRepository.ts
@@ -1,6 +1,15 @@
-import { type Season } from '@Multimedia/Season/domain/Season'
+import { Season } from '@Multimedia/Season/domain/Season'
 import { type SeasonRepository } from '@Multimedia/Season/domain/SeasonRepository'
+import { type Criteria } from '@Shared/domain/criteria/Criteria'
 import { MongoRepository } from '@Shared/infrastructure/persistence/mongo/MongoRepository'
+
+interface SeasonDocument {
+  _id: string
+  id: string
+  serieId: string
+  title: string
+  releaseDate: Date
+}
 
 export class MongoSeasonRepository
   extends MongoRepository<Season>
@@ -8,6 +17,18 @@ export class MongoSeasonRepository
 {
   public async save(season: Season): Promise<void> {
     await this.persist(season.id.value, season)
+  }
+
+  public async matching(criteria: Criteria): Promise<Season[]> {
+    const documents = await this.searchByCriteria<SeasonDocument>(criteria)
+    return documents.map((document) =>
+      Season.fromPrimitives({
+        id: document.id,
+        serieId: document.serieId,
+        title: document.title,
+        releaseDate: document.releaseDate
+      })
+    )
   }
 
   protected collectionName(): string {
