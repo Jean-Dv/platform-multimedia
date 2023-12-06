e8e6e82dac126c61d049c08961211d5ac83f11f6
Jean-Dv <mrjunior127@gmail.com>
2023-12-06 00:55:54 -0500

    feat: implement controller for get movie by id and routes

--- src/apps/multimedia/backend/routes/movies.route.ts
+++ src/apps/multimedia/backend/routes/movies.route.ts
@@ -1,28 +1,53 @@
-import { type Request, type Response, type Router } from 'express'
-import { body } from 'express-validator'
+import { type Router } from 'express'
+import { body, param } from 'express-validator'
 import { container } from '../dependency-injection'
 import { validateReqSchema } from '.'
 
-export function register(router: Router): void {
-  const reqSchemaOfCreateMovie = [
+function registerGetMovies(router: Router): void {
+  const controller = container.get(
+    'Apps.multimedia.controllers.MovieGetController'
+  )
+  router.get('/multimedia/movies', controller.run.bind(controller))
+}
+
+function registerPutMovie(router: Router): void {
+  const reqSchema = [
     body('id').exists().isString(),
     body('title').exists().isString(),
     body('releaseDate').exists().isISO8601().toDate(),
     body('duration').exists().isNumeric()
   ]
-  const moviePutController = container.get(
+  const controller = container.get(
     'Apps.multimedia.controllers.MoviePutController'
-  )
-  const movieGetController = container.get(
-    'Apps.multimedia.controllers.MovieGetController'
   )
   router.put(
     '/multimedia/movies/:id',
-    reqSchemaOfCreateMovie,
+    reqSchema,
     validateReqSchema,
-    (req: Request, res: Response) => moviePutController.run(req, res)
+    controller.run.bind(controller)
   )
-  router.get('/multimedia/movies', (req: Request, res: Response) =>
-    movieGetController.run(req, res)
+}
+
+function registerGetMovieById(router: Router): void {
+  const reqSchema = [param('id').isUUID()]
+  const authMiddleware = container.get(
+    'Apps.multimedia.middlewares.AuthenticateMiddleware'
   )
+  const controller = container.get(
+    'Apps.multimedia.controllers.MovieGetByIdController'
+  )
+  router.get(
+    '/multimedia/movies/:id',
+    reqSchema,
+    validateReqSchema,
+    authMiddleware.run,
+    reqSchema,
+    controller.run.bind(controller)
+  )
+}
+
+export function register(router: Router): void {
+  registerGetMovies(router)
+  registerPutMovie(router)
+  registerGetMovieById(router)
 }
