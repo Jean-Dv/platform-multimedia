ac927623973546af837211b8ccbb3d82cafd0162
Jean-Dv <mrjunior127@gmail.com>
2023-12-08 18:26:18 -0500

    feat: add Category domain files

---
+++ src/Contexts/Multimedia/Categories/domain/Category.ts
@@ -0,0 +1,43 @@
+import { AggregateRoot } from '@Shared/domain/AggregateRoot'
+import { CategoryId } from './CategoryId'
+import { CategoryName } from './CategoryName'
+import { CategoryCreatedDomainEvent } from './CategoryCreatedDomainEvent'
+
+export class Category extends AggregateRoot {
+  public readonly id: CategoryId
+  public readonly name: CategoryName
+
+  constructor(id: CategoryId, name: CategoryName) {
+    super()
+    this.id = id
+    this.name = name
+  }
+
+  public static create(id: CategoryId, name: CategoryName): Category {
+    const category = new Category(id, name)
+    category.record(
+      new CategoryCreatedDomainEvent({
+        aggregateId: id.value,
+        name: name.value
+      })
+    )
+    return category
+  }
+
+  public static fromPrimitives(plainData: {
+    id: string
+    name: string
+  }): Category {
+    return new Category(
+      new CategoryId(plainData.id),
+      new CategoryName(plainData.name)
+    )
+  }
+
+  public toPrimitives(): { id: string; name: string } {
+    return {
+      id: this.id.value,
+      name: this.name.value
+    }
+  }
+}
---
+++ src/Contexts/Multimedia/Categories/domain/CategoryName.ts
@@ -0,0 +1,17 @@
+import { StringValueObject } from '@Shared/domain/value-objects/StringValueObject'
+import { CategoryNameLengthIsExceeded } from './CategoryNameLengthIsExceeded'
+
+export class CategoryName extends StringValueObject {
+  constructor(value: string) {
+    super(value)
+    this.ensureLengthIsLessThan50Characters(value)
+  }
+
+  private ensureLengthIsLessThan50Characters(value: string): void {
+    if (value.length > 50) {
+      throw new CategoryNameLengthIsExceeded(
+        `The category name <${value}> has more than 50 characters`
+      )
+    }
+  }
+}
