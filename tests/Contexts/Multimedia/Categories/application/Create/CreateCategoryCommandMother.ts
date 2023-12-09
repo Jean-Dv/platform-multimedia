ce8e8d7e8192103f9b68bd8870adf964f847b010
Jean-Dv <mrjunior127@gmail.com>
2023-12-08 23:08:10 -0500

    refactor: import paths in CreateCategoryCommandMother.ts

--- tests/Contexts/Multimedia/Categories/application/Create/CreateCategoryCommandMother.ts
+++ tests/Contexts/Multimedia/Categories/application/Create/CreateCategoryCommandMother.ts
@@ -1,8 +1,8 @@
 import { type CreateCategoryCommand } from '@Multimedia/Categories/application/Create/CreateCategoryCommand'
 import { type CategoryId } from '@Multimedia/Categories/domain/CategoryId'
 import { type CategoryName } from '@Multimedia/Shared/domain/Category/CategoryName'
-import { CategoryIdMother } from '../domain/CategoryIdMother'
-import { CategoryNameMother } from '../domain/CategoryNameMother'
+import { CategoryIdMother } from '../../domain/CategoryIdMother'
+import { CategoryNameMother } from '../../domain/CategoryNameMother'
 
 export class CreateCategoryCommandMother {
   public static create(
