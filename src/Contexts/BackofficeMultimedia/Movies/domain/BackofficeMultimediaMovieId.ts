8fa12e5fcf470ed14ef36edb851b872eb9a7e7fa
Jean-Dv <mrjunior127@gmail.com>
2024-01-11 14:54:28 -0500

    feat: add new domain classes for BackofficeMultimediaMovies

---
+++ src/Contexts/BackofficeMultimedia/Movies/domain/BackofficeMultimediaMovieId.ts
@@ -0,0 +1,6 @@
+import { Uuid } from '@Shared/domain/value-objects/Uuid'
+
+/**
+ * Represents the ID of a backoffice multimedia movie.
+ */
+export class BackofficeMultimediaMovieId extends Uuid {}
