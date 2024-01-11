dafa7668424f4b2b431dcf99a51c8ce9fc02c3b1
Jean-Dv <mrjunior127@gmail.com>
2024-01-10 19:08:45 -0500

    feat: add MongoBackofficeCategoryRepository implementation

---
+++ src/Contexts/BackofficeMultimedia/Categories/infrastructure/persistence/MongoBackofficeCategoryRepository.ts
@@ -0,0 +1,16 @@
+import { MongoRepository } from '@Shared/infrastructure/persistence/mongo/MongoRepository'
+import { type BackofficeCategory } from '../../domain/BackofficeCategory'
+import { type BackofficeCategoryRepository } from '../../domain/BackofficeCategoryRepository'
+
+export class MongoBackofficeCategoryRepository
+  extends MongoRepository<BackofficeCategory>
+  implements BackofficeCategoryRepository
+{
+  public async save(category: BackofficeCategory): Promise<void> {
+    await this.persist(category.id.value, category)
+  }
+
+  public collectionName(): string {
+    return 'categories'
+  }
+}
