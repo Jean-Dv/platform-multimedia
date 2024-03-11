c82813a2567072672ccd0b43a3997719d6b6ad58
Jean-Dv <mrjunior127@gmail.com>
2024-03-11 17:55:50 -0500

    feat: add search and delete to movie repository

--- src/Contexts/BackofficeMultimedia/Movies/infrastructure/persistence/MongoBackofficeMultimediaMovieRepository.ts
+++ src/Contexts/BackofficeMultimedia/Movies/infrastructure/persistence/MongoBackofficeMultimediaMovieRepository.ts
@@ -1,6 +1,17 @@
 import { type BackofficeMultimediaMovieRepository } from './../../domain/BackofficeMultimediaMovieRepository'
-import { type BackofficeMultimediaMovie } from './../../domain/BackofficeMultimediaMovie'
+import { BackofficeMultimediaMovie } from './../../domain/BackofficeMultimediaMovie'
 import { MongoRepository } from '@Shared/infrastructure/persistence/mongo/MongoRepository'
+import { type BackofficeMultimediaMovieId } from '@BackofficeMultimedia/Movies/domain/BackofficeMultimediaMovieId'
+import { type UUID } from 'mongodb'
+
+interface BackofficeMultimediaMovieMongoDocument {
+  _id: UUID
+  title: string
+  releaseYear: number
+  synopsis: string
+  categories: string[]
+  videoId: string
+}
 
 export class MongoBackofficeMultimediaMovieRepository
   extends MongoRepository<BackofficeMultimediaMovie>
@@ -8,6 +19,28 @@ export class MongoBackofficeMultimediaMovieRepository
 {
   public async save(movie: BackofficeMultimediaMovie): Promise<void> {
     await this.persist(movie.id.value, movie)
+  }
+
+  public async delete(movie: BackofficeMultimediaMovie): Promise<void> {
+    await this.erase(movie.id.value)
+  }
+
+  public async search(
+    id: BackofficeMultimediaMovieId
+  ): Promise<BackofficeMultimediaMovie | null> {
+    const movie = await this.findById<BackofficeMultimediaMovieMongoDocument>(
+      id.value
+    )
+    return movie !== null
+      ? BackofficeMultimediaMovie.fromPrimitives({
+          id: movie._id.toString(),
+          title: movie.title,
+          releaseYear: movie.releaseYear,
+          synopsis: movie.synopsis,
+          categories: movie.categories,
+          videoId: movie.videoId
+        })
+      : null
   }
 
   protected collectionName(): string {
